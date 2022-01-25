e936327e1b355af853c278fc72cc77f28937e8c9
raymondcerneytech <cerne004@gmail.com>
2022-01-24 23:00:03 -0600

    working on indexdb and service-worker

--- public/js/indexdb.js
+++ public/js/indexdb.js
@@ -1,11 +1,60 @@
 function createIndexedDB() {
+	let db;
+
 	const request = indexedDB.open("budget-tracker", 1);
 
-	request.onupgradeneeded = e => {
-
+	request.onupgradeneeded = (e) => {
+		const db = e.target.result;
+		db.createObjectStore("transaction", { autoIncrement: true });
 	};
 
-	request.onsuccess = e => { };
+	request.onsuccess = (e) => {
+		const db = e.target.result;
 
-	request.onerror = e => { };
+		//checks if online and uploads
+		if (navigator.onLine) {
+			uploadTransaction();
+		}
+	};
+
+	request.onerror = (e) => {
+		console.log(e.target.errorCode);
+	};
+
+	function uploadTransaction() {
+		const transaction = db.transaction(["transaction"], "readwrite");
+		const transactionObjectStore = transaction.objectStore("transaction");
+		const getAll = transactionObjectStore.getAll();
+
+		getAll.onsuccess = function () {
+			if (getAll.result.length > 0) {
+				fetch("/api/transaction/bulk", {
+					method: "POST",
+					body: JSON.stringify(getAll.result),
+					headers: {
+						Accept: "application/json, text/plain, */*",
+						"Content-Type": "application/json",
+					},
+				})
+					.then((response) => response.json())
+					.then((serverResponse) => {
+						if (serverResponse.message) {
+							throw new Error(serverResponse);
+						}
+						const transaction = db.transaction(["transaction"], "readwrite");
+						const transactionObjectStore = transaction.objectStore("transaction");
+						transactionObjectStore.clear();
+
+						alert("All saved transactions have been submitted!");
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			}
+		};
+	}
+
+	window.addEventListener("online", uploadTransaction);
 }
+
+createIndexedDB();
--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'budget-tracker-v1';
 
 const FILES_TO_CACHE = ["./index.html", "./manifest.json", "./css/styles.css", "./icons/icon-72x72.png", "./icons/icon-96x96.png", "./icons/icon-128x128.png", "./icons/icon-144x144.png", "./icons/icon-152x152.png", "./icons/icon-192x192.png", "./icons/icon-384x384.png", "./icons/icon-512x512.png", "./js/indexdb.js", "./js/index.js"];
 
-self.addEventListener("install", function (e) {
+self.addEventListener('install', function (e) {
 	e.waitUntil(
 		caches
 			.open(CACHE_NAME)
